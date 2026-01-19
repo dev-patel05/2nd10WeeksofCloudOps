d1621397929f5ddbd4f191181e82b454f7f36e32
Dev <pdev444444@gmail.com>
2026-01-19 17:32:11 +0530

    Update API URL to HTTPS

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,3 +1,4 @@
 // const API_BASE_URL = "http://localhost:8800";
-const API_BASE_URL = "http://backend-alb-primary-50195916.us-east-1.elb.amazonaws.com";
+// const API_BASE_URL = "http://backend-alb-primary-50195916.us-east-1.elb.amazonaws.com";
+const API_BASE_URL = "https://api.pateldev.in";
 export default API_BASE_URL;
